a77f04ea1f27b4cbd24891055cd52b7739d1a013
brianlsy98 <101338415+brianlsy98@users.noreply.github.com>
2022-05-31 18:20:07 +0900

    Update GOTOMARS_project2.py

--- project/GOTOMARS_project2.py
+++ project/GOTOMARS_project2.py
@@ -23,7 +23,7 @@ import math
 import yaml
 import time
 from sim2real.msg import Result, Query
-from sklearn.utils import shuffle
+from sklearn.utils import shuffle, resample
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
 from sklearn.preprocessing import StandardScaler    # normalization
@@ -62,18 +62,18 @@ class GaussianProcess:
         
         self.demo_files = []
 
-        self.obs_num = 1000
+        self.obs_num = 500
 
         self.demo_obs = []
         self.demo_act = []
         self.demo_rwd = []
 
-        self.kernel = C(constant_value=1.0, constant_value_bounds=(1e-5, 10.0))* RBF(length_scale=0.5, length_scale_bounds=(1e-5, 10.0))
-
-        
+        self.kernel = C(constant_value=1.0, constant_value_bounds=(1e-5, 10.0))* RBF(length_scale=1.0, length_scale_bounds=(1e-5, 10.0)) + C(constant_value = 0.5, constant_value_bounds = (1e-5, 1.0)) * RBF(length_scale = 1.0, length_scale_bounds = (1e-5, 10.0))
         self.gp = GaussianProcessRegressor(kernel = self.kernel, alpha = 0.2)
         self.gp_file_name = "demo"
-        self.gp_file = project_path + "/" + self.gp_file_name + ".joblib"
+        self.gp_file = team_path + "/" + self.gp_file_name + ".joblib"
+        self.obs_file = team_path + "/obs_file.pkl"
+        self.act_file = team_path + "/act_file.pkl"
 
         self.load()
 
@@ -90,63 +90,81 @@ class GaussianProcess:
         3) if you already have a fitted model, load it instead of fitting the model again.
         Please implement loading pretrained model part for fast evaluation.
         """
-        if False:   # TODO: if fitted model exist
-            1
-        else:
+        try:   # if fitted model exist
+            self.gp = load(self.gp_file)
+            self.normalized_obs = load(self.obs_file)
+            self.normalized_act = load(self.act_file)
+            print('pre-trained model exists')
+        except:
+            print('no pre-trained model exists, start loading data')
             # 1) load expert demonstration
-            track1_1 = np.loadtxt(project_path+'/project/IS_GOTOMARS/project/expert_data_track_1_0.5&5.0.txt')
-            track1_2 = np.loadtxt(project_path+'/project/IS_GOTOMARS/project/expert_data_track_1_1.0&4.5.txt')
-            track1_3 = np.loadtxt(project_path+'/project/IS_GOTOMARS/project/expert_data_track_1_1.2&4.0.txt')
-            track1_4 = np.loadtxt(project_path+'/project/IS_GOTOMARS/project/expert_data_track_1_1.5&3.5.txt')
-            track1_5 = np.loadtxt(project_path+'/project/IS_GOTOMARS/project/expert_data_track_1_0.0&5.5.txt')
-            track2_1 = np.loadtxt(project_path+'/project/IS_GOTOMARS/project/expert_data_track_2_0.5&5.0.txt')
-            track2_2 = np.loadtxt(project_path+'/project/IS_GOTOMARS/project/expert_data_track_2_1.0&4.5.txt')
-            track2_3 = np.loadtxt(project_path+'/project/IS_GOTOMARS/project/expert_data_track_2_1.2&4.0.txt')
+            track1_1 = np.loadtxt(project_path+'/project/IS_GOTOMARS/project/map1_1')
+            track1_2 = np.loadtxt(project_path+'/project/IS_GOTOMARS/project/map1_2')
+            track1_3 = np.loadtxt(project_path+'/project/IS_GOTOMARS/project/map1_3')
+            track1_4 = np.loadtxt(project_path+'/project/IS_GOTOMARS/project/map1_4')
+            track1_5 = np.loadtxt(project_path+'/project/IS_GOTOMARS/project/map1_5')
+            track2_1 = np.loadtxt(project_path+'/project/IS_GOTOMARS/project/map2_1')
+            track2_2 = np.loadtxt(project_path+'/project/IS_GOTOMARS/project/map2_2')
+            track2_3 = np.loadtxt(project_path+'/project/IS_GOTOMARS/project/map2_3')
+            track2_4 = np.loadtxt(project_path+'/project/IS_GOTOMARS/project/map2_4')
+            track2_5 = np.loadtxt(project_path+'/project/IS_GOTOMARS/project/map2_5')
+            
             track1_total = np.append(track1_1, track1_2, axis=0)
             track1_total = np.append(track1_total, track1_3, axis=0)
             track1_total = np.append(track1_total, track1_4, axis=0)
             track1_total = np.append(track1_total, track1_5, axis=0)                                   
             track2_total = np.append(np.append(track2_1, track2_2, axis=0), track2_3, axis=0)
-            
-            self.demo_files = shuffle(np.append(track1_total, track2_total, axis=0)) # shuffle state action pairs
-            # self.demo_files = shuffle(track1_total)
-            self.demo_obs = np.clip(self.demo_files[:,:-5], 0, 30)  # prevent inf value
+            track2_total = np.append(track2_total,track2_4,axis=0)
+            track2_total = np.append(track2_total,track2_5,axis=0)
+            self.demo_files = np.append(track1_total, track2_total, axis=0)
+            self.demo_obs = np.clip(self.demo_files[:, :-5], 0, 30)     # prevent inf value
             self.demo_obs_action = np.array(self.demo_files[:,-5:-3])
             self.demo_act = np.array(self.demo_files[:,-3:-1])
             self.demo_rwd = np.array(self.demo_files[:,-1])
+            self.demo_obs = self.demo_obs.tolist()
+            self.demo_act = self.demo_act.tolist()
+
+            for i in range(len(self.demo_obs)):                         # append more datas for streering to the right side properly
+                if self.demo_obs[i][0] > 0:
+                    self.demo_obs.append(self.demo_obs[i])
+                    self.demo_obs.append(self.demo_obs[i])
+                    self.demo_obs.append(self.demo_obs[i])
+                    self.demo_act.append(self.demo_act[i])
+                    self.demo_act.append(self.demo_act[i])
+                    self.demo_act.append(self.demo_act[i])
+            self.demo_act = np.array(self.demo_act)
+            self.demo_obs = np.array(self.demo_obs)
+
             print(np.shape(self.demo_obs))
             print(np.shape(self.demo_obs_action))
             print(np.shape(self.demo_act))
             print(np.shape(self.demo_rwd))
-
-            ### downsizeing obs : 1083 -> 28 ###
-            downsize_obs = []
-            for i in range(len(self.demo_obs)):
-                raw_obs = []
-                for j in range(51):
-                    raw_obs.append(np.mean(self.demo_obs[i][20*j:20*j+81]))
-                # raw_obs.extend([self.demo_obs_action[i][0], self.demo_obs[i][1]])
-                downsize_obs.append(raw_obs)
-            ###------------------------------###  
             
+
             # 2) normalize
-            # obs
+            self.demo_obs, self.demo_act = resample(self.demo_obs, self.demo_act, n_samples = self.obs_num, replace = False)
             self.normalized_obs = StandardScaler()
             self.normalized_act = StandardScaler()
-            # self.normalized_obs.fit(downsize_obs)
             self.normalized_obs.fit(self.demo_obs)
             self.normalized_act.fit(self.demo_act)
-            # s = self.normalized_obs.transform(downsize_obs)
-            s = self.normalized_obs.transform(self.demo_obs)
-            a = self.normalized_act.transform(self.demo_act)
+            transformed_obs = self.normalized_obs.transform(self.demo_obs)
+            transformed_act = self.normalized_act.transform(self.demo_act)
+            dump(self.normalized_obs, self.obs_file)
+            dump(self.normalized_act, self.act_file)
+            
 
-            # 3) gp fit   
+            # 3) get gp fitted with normalized, shuffled datas
+            s, a = shuffle(transformed_obs, transformed_act)
             print("d : ", np.shape(s))
             print("a : ", np.shape(a))
             self.gp.fit(s, a)
             print(self.gp.score(s, a))
             print("fitted")
-            print("")
+            dump(self.gp, self.gp_file)
+
+        return
+
+        
 
 
 
@@ -160,15 +178,6 @@ class GaussianProcess:
         """
 
         obs_clip = np.clip(obs[:,:-2], 0, 30)
-        ### downsizeing obs : 1083 -> 28 ###
-        raw_obs = []
-        for i in range(51):
-            raw_obs.append(np.mean(obs_clip[0][20*i:20*i+81]))
-        # raw_obs.extend([obs[0][-2], obs[0][-1]])
-        ###------------------------------###
-
-        # normalize
-        # s = self.normalized_obs.transform([raw_obs])
         s = self.normalized_obs.transform(obs_clip)
         [[angle_act, linvel_act]] = self.gp.predict(s)
         i_steering, i_linear = self.normalized_act.inverse_transform([angle_act, linvel_act])
@@ -176,6 +185,7 @@ class GaussianProcess:
         print("ang_actvel : ", i_steering)
         print(obs_clip)
         print("")
+        
         return [[i_steering, i_linear]]
 
 
@@ -274,4 +284,3 @@ if __name__ == '__main__':
         args = yaml.load(file)
     GaussianProcess(args)
     rospy.spin()
-
